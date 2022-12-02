004a05ffd95f7de9aac90dd205c8e0aa3165a462
KawaiiZT <95923980+KawaiiZT@users.noreply.github.com>
2022-12-02 22:22:37 +0700

    Create schoolwork.py

---
+++ schoolwork.py
@@ -0,0 +1,20 @@
+price = 6.454897
+price = round(price,2)
+print(price)
+print(type(price))
+a = 'imagineering'
+print(a[0])
+print(a[-4])
+print(a[0:3])
+print(a[-4:-1])
+print(a[0:3]*3)
+name= "trisit"
+surname = "Charoenparipat"
+age=18
+print(len(name))
+print(len(surname))
+print(len('age'))
+a=12.4
+a=str(a)
+print(a)
+print(type(a))
