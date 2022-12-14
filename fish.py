70ba6306a8866185b7710ce5019d7180af482ebe
KawaiiZT <95923980+KawaiiZT@users.noreply.github.com>
2022-12-14 11:37:34 +0700

    Create fish.py

---
+++ fish.py
@@ -0,0 +1,12 @@
+fish =["salmon","cod","Shark","Whale"]
+print(fish[0])
+print(fish[3])
+print(fish[-1])
+print(len(fish))
+print(len(fish[-4]))
+print(type(fish))
+print(fish)
+fish[1]="Nemo"
+print(fish)
+fish.append("Cod")
+print(fish)
