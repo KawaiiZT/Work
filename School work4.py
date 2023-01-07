84d766daf80e711db0bebf5c1fa8ba6d98d67e93
KawaiiZT <95923980+KawaiiZT@users.noreply.github.com>
2023-01-07 21:10:41 +0700

    Create School work4.py

---
+++ School work4.py
@@ -0,0 +1,13 @@
+d='i MakE innoVAtion'
+print(d.capitalize())
+print(d.title())
+print(d.upper())
+print(d.lower())
+
+e='Lipda pola'
+print(e.cecnter(16))
+print(e.ljust(16))
+print(e.rjust(16))
+print(e.cecnter(16,'-'))
+print(e.ljust(16,'-'))
+print(e.rjust(16,'-'))
