153465d91ed05298f99e6c91ad10fdc4e37ef0f5
KawaiiZT <95923980+KawaiiZT@users.noreply.github.com>
2023-01-14 21:53:27 +0700

    Create schoolwork3.py

---
+++ schoolwork3.py
@@ -0,0 +1,15 @@
+f='Lip'
+print(f.strip())
+print(f.lstrip())
+print(f.rstrip())
+
+print('I make innovation' .isalnum())
+print('Lipda pola'.isalpha())
+print('2022'.isnumeric())
+print('i make innovation'.islower())
+print('I MAKE INNOVATION'.isupper())
+
+lang=['C','Java','python','vb']
+print(','.join(lang))
+lang2='C Java Python vb'
+print(lang2.split(' '))
