202095be1c3738b3185141e3492f8819412ce86b
KawaiiZT <95923980+KawaiiZT@users.noreply.github.com>
2023-01-17 23:38:55 +0700

    Update schoolwork3.py

--- schoolwork3.py
+++ schoolwork3.py
@@ -13,3 +13,25 @@ lang=['C','Java','python','vb']
 print(','.join(lang))
 lang2='C Java Python vb'
 print(lang2.split(' '))
+
+classitem=['Pencil','eraser','ruler']
+classitem.append('board')
+print(classitem)
+classitem.insert(1,'pen')
+print(classitem)
+classitem.remove('ruler')
+print(classitem)
+print(classitem.count('eraser'))
+classitem.sort()
+print(classitem)
+
+menu_a=['salad','fruit','broccoli']
+menu_b=['meat','fish','pork']
+print('Choose menu a or b')
+select=input()
+if select == 'a':
+    print('menu a:',menu_a)
+if select == 'b':
+    print('menu b:',menu_b)
+else:
+    print('no this menu is not in the menu list')
