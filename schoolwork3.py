6572581bbc403e820796022d6732cfa162e6dbd4
KawaiiZT <95923980+KawaiiZT@users.noreply.github.com>
2023-01-25 00:08:35 +0700

    Update schoolwork3.py

--- schoolwork3.py
+++ schoolwork3.py
@@ -77,3 +77,12 @@ forward(100)
 right(90)
 forward(100)
 right(90)
+
+from turtle import*
+color=['red','purple','blue','green','yellow','orange']
+for x in range(360):
+    pencolor(color[x%6])
+    width(x/100+1)
+    forward(x)
+    left(59)
+
